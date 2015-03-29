80ec116321c298ab9ba10a8049839b5f7a0f0f47
ivzaitsev <ivzaitsev@gmail.com>
2015-03-29 15:30:54 +0400

    Added FileFetcher usage, redirects

--- src/net/aixm/ControllerServlet/ControllerServlet.java
+++ src/net/aixm/ControllerServlet/ControllerServlet.java
@@ -1,66 +1,52 @@
 package net.aixm.ControllerServlet;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.aixm.FileFetcher.*;
 
-/**
- * Servlet implementation class ControllerServlet
- */
-@WebServlet(urlPatterns="/",loadOnStartup = 1)
+@WebServlet(urlPatterns={"/","/display"},loadOnStartup = 1)
 public class ControllerServlet extends HttpServlet {
 
-    private static final String SAVE_DIR = "uploads";
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
 
-        try {
-        	System.out.println("constrollerservlet");
-        	ArrayList files = new ArrayList();
-            String appPath = request.getServletContext().getRealPath("");                       
-            File fileFolder = new File(appPath+ File.separator+ SAVE_DIR);
-        	
-        	response.setContentType("application/json");
-        	response.setCharacterEncoding("UTF-8");
-        	//files.add(fileFolder);
-
-            for (File f : fileFolder.listFiles()) { // Line 27
-                files.add(f.getName());
-                System.out.println(f.getName());
-            }
-            request.setAttribute("uploadedFiles",files);
-            request.setAttribute("uploadDir",fileFolder);
-
-        	request.getRequestDispatcher("/main.jsp").forward(request, response);
-
-        	
-        } catch (Exception ex) {
+        try 
+        {
+        	System.out.println("constrollerservlet getting files list");
+        	FileFetcher ftch= new FileFetcher(request,response);
+            request.setAttribute("uploadedFiles",ftch.GetFiles());
+            System.out.println("constrollerservlet redirecting to main");
+        	request.getRequestDispatcher("main.jsp").forward(request, response);
+        }	
+        catch (Exception ex) 
+        {
             ex.printStackTrace();
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-
-    	try {
-
-            request.getRequestDispatcher("/main.jsp").forward(request, response);
-         } catch (Exception ex) {
+    throws ServletException, IOException 
+    {
+    	try 
+    	{
+    		System.out.println("constrollerservlet post getting files list");
+        	FileFetcher ftch= new FileFetcher(request,response);
+            request.setAttribute("uploadedFiles",ftch.GetFiles());
+            System.out.println("constrollerservlet post redirecting to main");
+            request.getRequestDispatcher("main.jsp").forward(request, response);
+         } 
+    	catch (Exception ex) 
+    	{
              ex.printStackTrace();
          }
     }
